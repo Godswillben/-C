955e21b84dcdf09e3d28a1d279d8e994fdf39b7d
Godswillben <amogugodswill99@gmail.com>
2023-02-18 10:10:57 +0100

    c fundamentals

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    print("I like pizza!\n");
+
+    // this is a comment
+    /*
+    This one 
+    is 
+    multiline
+    */
+
+    printf("I like pizza!\n");
     printf("It's really good!\n");
     return 0;
 }
